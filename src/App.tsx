5303b84368dde153094fc0d4efff2bdbfb5a4c41
nasvank7 <nasvank7@gmail.com>
2025-11-27 00:32:19 +0530

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
